dcbdc5127fb772a286a7e655d5b8d437d7ec4d2e
Enzo <82385947+treasurestar@users.noreply.github.com>
2024-09-11 21:39:09 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,24 @@
+# operadores
+valorA = float(input("digite o valor de A: "))
+valorB = float(input("digite o  valor de B: "))
+# soma
+soma = valorA + valorB
+print(f"a soma foi = {soma}")
+# subtração
+subtracao = valorA - valorB
+print(f"a subtração foi = {subtracao}")
+# multiplicação
+multiplicacao = valorA * valorB
+print(f"a multiplicação = {multiplicacao}")
+# divisão
+divisao = valorA / valorB
+print(f"a divisão foi {divisao}")
+# div inteira
+divisao_inteira = valorA // valorB
+print(f"a divisão inteira = {divisao_inteira}")
+# resto da div
+resto_divisao = valorA % valorB
+print(f"o resto foi = {resto_divisao}")
+# exponenciação
+exponenciacao = valorA ** valorB
+print(f"a exponenciação foi = {exponenciacao}")
