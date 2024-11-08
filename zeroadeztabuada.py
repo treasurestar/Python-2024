b96a52c845a567fb19274b07ce3d42b670253a6e
Enzo <82385947+treasurestar@users.noreply.github.com>
2024-11-07 21:22:33 -0300

    Update zeroadeztabuada.py

---
+++ zeroadeztabuada.py
@@ -0,0 +1,9 @@
+num = 1
+while num <= 10:
+    i = 1  
+    print(f"Tabuada do {num}:")  
+    while i <= 10:  
+        print(f"{num} x {i} = {num * i}")  
+        i += 1  
+    print()  
+    num += 1 
